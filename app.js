f182f2771e5da01c435f304e347c3eca10623a4b
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-09-25 19:53:17 +0530

    Add window

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+// Initialization for ES Users
+import { Ripple, initMDB } from "mdb-ui-kit";
+
+initMDB({ Ripple });
+
 var t = setInterval(() => {
     var blink = document.getElementById('blink');
     blink.style.visibility = (blink.style.visibility == 'hidden' ? '' : 'hidden');
