c9af001e4594fea8871cbc2274bd47afa6c4898d
tejasnasa <138145714+tejasnasa@users.noreply.github.com>
2024-09-26 20:09:36 +0530

    Add title animation

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+var typed = new Typed('#typewrite', {
+    strings: ['Tejas Nasa'],
+    typeSpeed: 70,
+    onComplete: function() {
+        // Start the blinking effect after the typing is completed
+        var t = setInterval(() => {
+            var blink1 = document.querySelector('#blink1');
+            blink1.style.visibility = (blink1.style.visibility == 'hidden' ? '' : 'hidden');
+        }, 500);
+    }
+});
+
 var t = setInterval(() => {
-    var blink1 = document.querySelector('#blink1');
     var blink2 = document.querySelector('#blink2');
-    blink1.style.visibility = (blink1.style.visibility == 'hidden' ? '' : 'hidden');
     blink2.style.visibility = (blink2.style.visibility == 'hidden' ? '' : 'hidden');
 }, 500);
